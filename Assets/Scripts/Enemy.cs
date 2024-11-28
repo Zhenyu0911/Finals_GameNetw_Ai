4a6be5215d6c7a21dd33640ab6accac4a2f19c2a
onlyBibiesca <vikktourr@gmail.com>
2024-11-28 20:56:44 +0800

    Added AI Behaviour

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,8 +7,40 @@ using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun.Demo.Asteroids;
+using Unity.VisualScripting;
 
 public class Enemy : MonoBehaviourPunCallbacks
 {
-    
+    [SerializeField] public float MaxHP = 100.0f; //MaxHealth of the enemy
+    [SerializeField] public int CurrentHP; //Set HP
+    [SerializeField] public float Damage = 20.0f; //DamageEnemy
+    [SerializeField] public float CritRate = 1.2f; //increase of rate in hitting
+    [SerializeField] public float CritDMG; //increase rate of DMG
+    [SerializeField] public float HitChance = 6.0f; //default setting of chance (60/40)
+
+    void HitChance
+    {
+
+    }
+
+    void TakeDamage
+    {
+
+    }
+
+    void AttackDamage
+    {
+
+    }
+
+    void RandomCritRate
+    {
+
+    }
+
+    void RandomCritDamage
+    {
+
+    }
+      
 }
